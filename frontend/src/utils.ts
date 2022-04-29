5140097a62f413392a11977fc6d939c9a60fab15
Daniel Leal <dlleal@uc.cl>
2022-04-29 19:05:24 -0400

    Arrange the payment view

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -3,3 +3,11 @@ import type { DateTime } from 'luxon';
 export const formatDateTime = (dateTime: DateTime) => dateTime.toFormat('HH:mm:ss - dd/LL/yyyy');
 
 export const formatMoney = (money: number) => money.toLocaleString('es-CL');
+
+export const cleanNumber = (possibleNumber: string) => {
+  if (!possibleNumber) {
+    return '';
+  }
+
+  return possibleNumber.replace(/[^0-9]/g, '');
+};
--- frontend/src/views/PayView.vue
+++ frontend/src/views/PayView.vue
@@ -1,5 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { cleanNumber } from '@/utils';
+
+const amount = ref('');
+
+const input = (event: Event) => {
+  amount.value = cleanNumber((event.target as HTMLInputElement)?.value);
+};
+
+const pay = () => {
+  console.log('Payin\'...');
+};
+</script>
+
 <template>
-  <v-container class="mt-6">
-    Home
+  <v-container class="h-100 d-flex flex-column justify-end">
+    <v-form class="mb-3">
+      <v-text-field
+        v-model="amount"
+        label="Amount"
+        variant="outlined"
+        class="mb-4"
+        hide-details
+        @input="input"
+      />
+      <div class="w-100 d-flex justify-end">
+        <v-btn
+          size="large"
+          @click="pay"
+        >
+          Pay!
+        </v-btn>
+      </div>
+    </v-form>
   </v-container>
 </template>
+
+<style scoped>
+.h-100 {
+  height: 100%;
+}
+
+.w-100 {
+  width: 100%;
+}
+</style>
